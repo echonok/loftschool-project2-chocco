d1513d1e63e42f30f08a85d88ae6b4f4f659d762
Nick Elovsky <echonok@gmail.com>
2019-12-06 12:17:05 +0300

    изменил бургер

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const menuButton = document.querySelector('.bar__icon');
 const menuList = document.querySelector('.bar__ingredients');
 
-menuButton.addEventListener('click', () => {
+menuButton.addEventListener('click', (elem) => {
+  elem.preventDefault();
   menuList.style.display = menuList.style.display === 'block' ? 'none' : 'block';
+});
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const hamburger = document.querySelector('#hamburger-animated');
+hamburger.addEventListener('click', (elem) => {
+  elem.preventDefault();
+  hamburger.classList.toggle('open');
 });
