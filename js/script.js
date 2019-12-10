371360109c63fdfb36e11c3bc54784caa94f4072
Nick Elovsky <echonok@gmail.com>
2019-12-10 15:47:13 +0300

    добавил модальное окно

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const menuButton = document.querySelector('.bar__icon');
 const menuList = document.querySelector('.bar__ingredients');
-
 menuButton.addEventListener('click', (elem) => {
   elem.preventDefault();
   menuList.style.display = menuList.style.display === 'block' ? 'none' : 'block';
@@ -15,7 +14,6 @@ hamburger.addEventListener('click', (elem) => {
 });
 
 const accordItems = document.querySelectorAll('.vertical-accord__item');
-
 accordItems.forEach(item => {
 
   const accordTrigger = item.querySelector('.vertical-accord__trigger');
@@ -46,4 +44,89 @@ const loop = (direction, elem) => {
   } else {
     barList.insertBefore(barList.lastElementChild, barList.firstElementChild);
   }
+};
+
+const orderForm = document.querySelector('#order-form');
+const orderButton = document.querySelector('#order-button');
+const closeButton = document.querySelector('#close-button');
+const modalWindow = document.querySelector('.modal');
+
+orderButton.addEventListener('click', (elem) => {
+  elem.preventDefault();
+  
+  if (validateForm(orderForm)) {
+    
+    const myMail = 'echonok@gmail.com';
+
+    const formData = new FormData();
+    formData.append('name', orderForm.elements.name.value);
+    formData.append('phone', orderForm.elements.phone.value);
+    formData.append('comment', orderForm.elements.comment.value);
+    formData.append('to', myMail);
+
+    sendDataFetch(formData);
+
+  }
+});
+
+const sendDataFetch = async (formData) => {
+  try {
+    const response = await fetch('https://webdev-api.loftschool.com/sendmail', {
+      method: 'POST',
+      body: formData
+    });
+    const result = await response.json();
+    console.log('Успех:', JSON.stringify(result));
+    modalWindow.style.display = 'block';
+    //modalWindow.style.textContent = result.message;    
+  } catch (error) {
+    console.error('Ошибка:', error);
+  }
+};
+
+const sendDataXML = (formData) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+  xhr.send(formData);
+  xhr.addEventListener('load', () => {
+    //debugger;
+    console.log(xhr.response);
+    console.log(JSON.parse(xhr.response).message);
+    modalWindow.style.display = 'block';
+    //JSON.parse(xhr.response)
+  })
+};
+
+closeButton.addEventListener('click', () => {
+  modalWindow.style.display = "none";
+})
+
+window.addEventListener('click', (elem) => {
+  if (event.target === modalWindow) {
+    modalWindow.style.display = "none";
+  }
+})
+
+const validateForm = (form) => {
+  let valid = true;
+  
+  if (!validateField(form.elements.name)) {
+    valid = false;
+  }
+
+  if (!validateField(form.elements.phone)) {
+    valid = false;
+  }
+
+  if (!validateField(form.elements.comment)) {
+    valid = false;
+  }
+
+  return valid;
+};
+
+const validateField = (field) => {
+  //debugger;
+  field.nextElementSibling.textContent = field.validationMessage;
+  return field.checkValidity();
 };
